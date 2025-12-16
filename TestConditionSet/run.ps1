0941ed40dc219b2dff8d91ab4b55a6e0a2ea0eed
Peter Kaagman <prjv.kaagman@gmail.com>
2025-12-16 20:20:04 +0100

    stap 1

--- TestConditionSet/run.ps1
+++ TestConditionSet/run.ps1
@@ -2,11 +2,22 @@ param($req, $TriggerMetadata)
 
 function Run-Function {
     param($req, $TriggerMetadata)
-    # TODO: implement logic step by step
     Write-Information "Skeleton function called"
+    if ($null -ne $req.Body) {
+        Write-Information "Body ontvangen"
+    } else {
+        Write-Information "Geen body ontvangen"
+        # Raise error or handle accordingly
+        Push-OutputBinding -Name res -Value ([HttpResponseContext]@{
+            StatusCode = 400
+            Body = @{ message = "Geen body ontvangen" }
+        })
+        return
+    }
+
     Push-OutputBinding -Name res -Value ([HttpResponseContext]@{
         StatusCode = 200
-        Body = @{ message = "Skeleton function executed" }
+        Body = @{ message = "Skeleton function werkt" }
     })
 }
 
